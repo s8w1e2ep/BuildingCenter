95b5bd30dccf3153b108070beb1cb5f6673c4f54
JiaYing <s97531246@gmail.com>
2017-07-18 22:49:58 +0800

    roughly layout

---
+++ BuildingCenter/MapViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MapViewController.swift
+//  BuildingCenter
+//
+//  Created by 李佳穎 on 18/07/2017.
+//  Copyright © 2017 uscc. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
