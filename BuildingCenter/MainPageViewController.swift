9be5f3c16a0e69f18ab7638cad02e9a5840015d1
JiaYing <s97531246@gmail.com>
2017-08-08 20:21:24 +0800

    Delete tabbarcontroller

---
+++ BuildingCenter/MainPageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainPageViewController.swift
+//  BuildingCenter
+//
+//  Created by 李佳穎 on 08/08/2017.
+//  Copyright © 2017 uscc. All rights reserved.
+//
+
+import UIKit
+
+class MainPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
