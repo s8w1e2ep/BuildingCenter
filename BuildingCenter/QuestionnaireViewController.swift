298cb4fa94c15a42a952cce4d346631b6ea2eb1f
wen326 <benson326789@gmail.com>
2017-08-22 10:15:58 +0800

    Add two Suggestion View

---
+++ BuildingCenter/QuestionnaireViewController.swift
@@ -0,0 +1,36 @@
+//
+//  QuestionnaireViewController.swift
+//  BuildingCenter
+//
+//  Created by wen on 2017/8/21.
+//  Copyright © 2017年 uscc. All rights reserved.
+//
+
+import UIKit
+
+class QuestionnaireViewController: UIViewController {
+
+    @IBOutlet var navbar: UINavigationBar!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    navbar.barTintColor = UIColor.white
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
