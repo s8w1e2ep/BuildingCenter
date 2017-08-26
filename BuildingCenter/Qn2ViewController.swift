0a5899030726885a4f44d34499921fca2f5ce36d
wen326 <benson326789@gmail.com>
2017-08-26 10:27:57 +0800

    Questionnaire layout complete

---
+++ BuildingCenter/Qn2ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  Qn2ViewController.swift
+//  BuildingCenter
+//
+//  Created by wen on 2017/8/24.
+//  Copyright © 2017年 uscc. All rights reserved.
+//
+
+import UIKit
+
+class Qn2ViewController: UIViewController {
+
+    @IBOutlet var navbar: UINavigationBar!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navbar.barTintColor = UIColor.white
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BuildingCenter/Qn7ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  Qn7ViewController.swift
+//  BuildingCenter
+//
+//  Created by wen on 2017/8/26.
+//  Copyright © 2017年 uscc. All rights reserved.
+//
+
+import UIKit
+
+class Qn7ViewController: UIViewController {
+
+    @IBOutlet var navbar: UINavigationBar!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navbar.barTintColor = UIColor.white
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
