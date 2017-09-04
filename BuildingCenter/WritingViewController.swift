469696cbfb765df8ffc64368e716e9d82057c01d
Reack <gon5483@gmail.com>
2017-09-04 15:28:44 +0800

    add selects and writing

---
+++ BuildingCenter/WritingViewController.swift
@@ -0,0 +1,38 @@
+//
+//  WritingViewController.swift
+//  BuildingCenter
+//
+//  Created by Chi Li on 2017/9/4.
+//  Copyright © 2017年 uscc. All rights reserved.
+//
+
+import UIKit
+
+class WritingViewController: UIViewController {
+
+    
+    @IBOutlet weak var imageView: UIImageView!
+    var image: UIImage!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
