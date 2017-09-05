64895d9d4f3235925c4ff67ec629a633ed5fe634
JiaYing <s97531246@gmail.com>
2017-09-05 17:26:31 +0800

    Finish ModeContent pageview prototype

---
+++ BuildingCenter/ModeContentDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ModeContentDetailViewController.swift
+//  BuildingCenter
+//
+//  Created by 李佳穎 on 05/09/2017.
+//  Copyright © 2017 uscc. All rights reserved.
+//
+
+import UIKit
+
+class ModeContentDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
