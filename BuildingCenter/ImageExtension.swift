c51c37989ccc9b59cb08d8a26b21e37d2738c364
s8w1e2ep <ha9mv8c@gmail.com>
2017-09-12 15:50:30 +0800

    Finish query zone mode device company table and adding image extension

---
+++ BuildingCenter/ImageExtension.swift
@@ -0,0 +1,33 @@
+//
+//  ImageExtension.swift
+//  BuildingCenter
+//
+//  Created by uscc on 2017/9/12.
+//  Copyright © 2017年 uscc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+    // use : imageView.downloadedFrom(link: "http://www.apple.com/euro/ios/ios8/a/generic/images/og.png")
+
+    func downloadedFrom(url: URL, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() { () -> Void in
+                self.image = image
+            }
+            }.resume()
+    }
+    func downloadedFrom(link: String, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloadedFrom(url: url, contentMode: mode)
+    }
+}
